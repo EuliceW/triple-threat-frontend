9c278e6f79cd6844f91bf618227d5d74fcd4e8de
Eulice Winston IV <64444880+EuliceW@users.noreply.github.com>
2022-11-09 10:07:43 -0600

    Screens
    These are the frontend screen you will use to build your screens. You can use the Home.tsx file as a reference.

---
+++ tsu-software-engineering-react/src/Views/AccessLog.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function AccessLog() {
+
+    return (
+      <div>
+        {/* Access Log text */}
+        <h2 className="is-size-2 pb-6 has-text-weight-medium">Access Log</h2>
+      </div>
+    );
+}
+
+export default AccessLog;
---
+++ tsu-software-engineering-react/src/Views/SearchClient.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function SearchClient() {
+
+    return (
+      <div>
+        {/* Search Clienth text */}
+        <h2 className="is-size-2 pb-6 has-text-weight-medium">Search Client</h2>
+      </div>
+    );
+}
+
+export default SearchClient;
