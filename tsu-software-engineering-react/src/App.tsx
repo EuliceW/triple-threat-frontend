f8fb8bb00da429b1a90de7d1d1e310b7a1f44297
Eulice Winston IV <64444880+EuliceW@users.noreply.github.com>
2022-11-09 10:14:06 -0600

    Wrote in the paths to new screens

--- tsu-software-engineering-react/src/App.tsx
+++ tsu-software-engineering-react/src/App.tsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import About from "./Views/About";
 import Home from "./Views/Home";
+import SearchClient from "./Views/SearchClient";
+import AddClient from "./Views/AddClient";
+import AccessLog from "./Views/AccessLog";
 
 import React from "react";
 import {
@@ -16,6 +19,9 @@ function App() {
           <Route path="/">
             <Route index element={<Home/>}></Route>
             <Route path="about" element={<About/>}></Route>
+            <Route path="search" element={<SearchClient/>}></Route>
+            <Route path="add-client" element={<AddClient/>}></Route>
+            <Route path="access-log" element={<AccessLog/>}></Route>
           </Route>
         </Routes>
     </Router>
