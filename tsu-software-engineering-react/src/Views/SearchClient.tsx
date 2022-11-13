9b39506d2dce26e0d33240656eeb96ed4fa9f51f
Eulice Winston IV <64444880+EuliceW@users.noreply.github.com>
2022-11-13 12:47:56 -0600

    Update SearchClient.tsx

--- tsu-software-engineering-react/src/Views/SearchClient.tsx
+++ tsu-software-engineering-react/src/Views/SearchClient.tsx
@@ -1,11 +1,17 @@
 import "../App.css";
+import SearchBar from "./Components/SearchBar";
+
 
 function SearchClient() {
 
     return (
       <div>
-        {/* Search Clienth text */}
+        {/* Search Client text */}
         <h2 className="is-size-2 pb-6 has-text-weight-medium">Search Client</h2>
+        {/* Search Bar */}
+        <div className="App">
+          <SearchBar placeHolder="Enter a CLient Name, Number, and State"/>
+        </div>
       </div>
     );
 }
