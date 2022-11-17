098430b49a53525d445da2ebf8b4c6aaa9bb44a5
Eulice Winston IV <64444880+EuliceW@users.noreply.github.com>
2022-11-17 10:00:29 -0600

    "Add Inventory" screen file

---
+++ tsu-software-engineering-react/src/Views/AddInventory.tsx
@@ -0,0 +1,13 @@
+import "../App.css";
+
+function AddInventory() {
+
+    return (
+      <div>
+        {/* Add Inventory text */}
+        <h2 className="is-size-2 pb-6 has-text-weight-medium">Add Inventory</h2>
+      </div>
+    );
+}
+
+export default AddInventory;
